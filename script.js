654528269e3ce5c3efaeba25b4a84f7e60bab691
Emiliano Duque <duqueemiliano64@gmail.com>
2025-07-19 04:16:34 -0300

    Lo que hiciste hoy

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const navOptions = document.getElementById('navOptions');
 const closeMenu = document.getElementById('closeMenu');
 
 menuBtn.addEventListener('click', () => {
-    navOptions.classList.remove('hidden');
+    navOptions.classList.add('show');
 });
 
 closeMenu.addEventListener('click', () => {
-    navOptions.classList.add('hidden');
+    navOptions.classList.remove('show');
 });
